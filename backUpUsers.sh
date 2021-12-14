ad94cd66a34c90dc5d734166f2779530f2ceb71d
simongodb <77750987+simongodb@users.noreply.github.com>
2021-12-14 17:07:10 +0000

    first push

---
+++ backUpUsers.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# COMMENT BEFORE RUNNING CODE
+# THE SCRIPT WILL BEHAVE AS DESCRIBED BELOW:
+# 1- THE SCRIP WILL TAKE AN ARGUMENT (A TEXT FILE)
+# WHICH IS SUPPOSED TO HAVE A LIST OF USER NAMES
+# 2- WE WILL LOOP THROUG THE LIST ADN VERIFY WHETHER:
+# - THE USER EXISTS
+# - IF .BACKUP EXISTS
+# IF NOT WE WON'T PERFORM ANY BACKUPS
+# 2- IF CONDITIONS ARE MET WE WILL: 
+# CHECK IF BACKUP.TAR EXIST IN /VAR AND EXTRACT IT TO /TMP/BACKUP
+# LOOP THROUGH FILE IN HOME DIRECTORY AND SEE IF: 
+# - FILES IN HOME VS TMP/BACKUP/USER ARE SIMILAR
+# IF YES, CHANGE FILE NAME AND COPY LATEST VERSION OF FILE FROM HOME TO TMP
+# IF NO, COPY LATEST VERISON OF FILES FROM HOME TO TMP...
+# 3- COMPRESS USER BACKUP AND MV TO ?VAR
+
+
+if [ -z "$1" ]
+then
+  echo "No arguments or incorrect argument(s) supplied. Please run with users.txt file"
+  exit 1
+fi
+
+USERS_FILE=$1
+
+# declaring array of users
+declare -a ARRAY_USERS
+
+
+#stdin replaced with a file supplied as a first argument
+exec < $1
+let count=0
+
+while read LINE; do
+   ARRAY_USERS[$count]=$LINE
+   ((count++))
+done
+
+echo Number of users: ${#ARRAY_USERS[@]}
+
+#echo array's content
+echo ${ARRAY_USERS[@]}
+
+for USER in "${ARRAY_USERS[@]}"
+do
+  echo "BACKUP OF ${USER}"
+  USER_HOME=/home/${USER}
+
+  # Verif user exists
+  if [[ -d "${USER_HOME}" ]]
+  then
+    echo "USER path is ${USER_HOME}"
+    # Create /tmp/backup/<user>
+    mkdir -p "/tmp/backup/${USER}"
+    
+    # Verif backup exists
+    if [[ -f "${USER_HOME}/.backup" ]]
+    then
+      BACKUP_FILE="${USER_HOME}/.backup"
+      ls ${USER_HOME} > ${BACKUP_FILE}
+      echo ".backup path is ${BACKUP_FILE}"
+
+    # extract backup.tar to /tmp/backup
+      if [ -f /var/backup.tar ]; then
+        sudo tar -xf /var/backup.tar -C /tmp/backup/
+        echo "backup.tar extracted to /tmp/backup"
+      fi
+
+      # loop through home directory and see uf files are diff with /tmp/backup/user
+      ls ${USER_HOME} | while read FILE_NAME; do
+        if [ -f "/tmp/backup/${USER}/${FILE_NAME}" ]; then
+          DIFF=1
+          cmp -s "tmp/backup/${USER}/${FILE_NAME}" "${USER_HOME}/${FILE_NAME}"
+          DIFF=$?
+  
+          if [ ${DIFF} -ne 0 ]; then
+            INDEX=1
+            while [ -f "/tmp/backup/${USER}/${FILE_NAME}.${INDEX}" ]; do
+              let INDEX=INDEX+1
+            done
+            # change file name adding+1  and copy latest version of file from home
+            mv -v "/tmp/backup/${USER}/${FILE_NAME}" "/tmp/backup/${USER}/${FILE_NAME}.${INDEX}"  
+            cp "${USER_HOME}/${FILE_NAME}" "/tmp/backup/${USER}"
+            echo "Files from HOME moved to /tmp/backup/${USER} directory"
+          fi
+        else
+        cp "${USER_HOME}/${FILE_NAME}" "/tmp/backup/${USER}"
+        echo "Files from HOME moved to /tmp/backup/${USER} directory - FIRST VERSION"
+        fi  
+      done
+      # Gzip to /var/backup.tar.gz
+          cd /tmp/backup/
+          tar -czf "backup.tar" "${USER}" 
+          sudo mv "backup.tar" /var
+          echo "backup has been Gzip to /var"     
+    else
+      echo "${USER_HOME}/.backup does not exist, creating file with 0 value"
+      touch ${USER_HOME}/.backup
+    fi  
+  else
+    echo "${USER_HOME} does not exist"
+  fi  
+  sleep 1
+done
